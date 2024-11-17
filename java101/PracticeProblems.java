14e9eaaa80e92d0307b50b08c05e8eb87a5fdae4
Linze <dacoolboime@gmail.com>
2024-11-17 13:57:07 -0500

    oops forgot to save my vscode file first

--- java101/PracticeProblems.java
+++ java101/PracticeProblems.java
@@ -1,7 +1,7 @@
 public class PracticeProblems {
 	  public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,1,2,3,4,2,2};
-        int n = 2;
+        int[] arr = {1,2,3,4,5,1,3,3,4,5,2};
+        int n = 3;
         System.out.println(countOccurrences(arr,n));
     }
 
@@ -13,5 +13,70 @@ public class PracticeProblems {
             }
         }
         return count;
+    }
+}
+
+public class PracticeProblems {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int[] reverseArr= reverseArray(arr);
+
+        for (int num : reverseArr) {
+            System.out.println(num);
+        }
+}
+public static int[] reverseArray(int[] arr) {
+    int[] reverse = new int[arr.length];
+    for(int i = 0; i < arr.length;i++) {
+        reverse[i]= arr[arr.length - 1 - i];
+        }
+        return reverse;
+    }
+}  
+    
+public class PracticeProblems {
+    public static void main(String [] args) {
+        double[][] grid = {
+            {1.1, 2.2, 3.3},
+            {4.0, 5.0, 6.0},
+            {7.4, 8.5, 9.6}
+        };
+
+         double total = sumGrid(grid);
+        System.out.println(total);
+    }
+
+    public static double sumGrid(double[][] grid) {
+        double sum = 0.0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                sum += grid[i][j]; 
+            }
+        }
+        return sum;
+    }
+}
+
+public class PracticeProblems {
+    public static void main(String [] args) {
+       int n = 5;
+       int result = fib(n);
+       System.out.println(result); 
+    }
+
+public static int fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+ 
+    int a = 0, b = 1;
+    int fib = 0;
+
+    for (int i = 2; i <= n; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    return fib;
     }
 }
