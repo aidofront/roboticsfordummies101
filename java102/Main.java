be02e306b05dfe017beb8403da43ccdf88002542
Linze <dacoolboime@gmail.com>
2024-12-01 21:44:38 -0500

    added the practice questions for java102, and my work for them

---
+++ java102/Main.java
@@ -0,0 +1,11 @@
+package java102;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a new Point object
+        Point point = new Point(4, 3);
+
+        // Print out the x and y coordinates
+        System.out.println("x: " + point.x + ", y: " + point.y);
+    }
+}
---
+++ java102/Square.java
@@ -0,0 +1,70 @@
+package java102;
+
+public class Square {
+	public final Point corner;
+	public final double sideLength;
+
+	/**
+	* @param corner The bottom left corner of the square
+	* @param sideLength
+	*/
+	public Square(Point corner, double sideLength) {
+		this.corner = corner;
+		this.sideLength = sideLength;
+	}
+
+	public double area() {
+		return sideLength * sideLength;
+	}
+	
+	public double perimeter() {
+		return 4 * sideLength;
+	}
+	
+	/** 
+	* @return Whether point p is inside of the square.
+	*/
+	public boolean isInside(Point p) {
+		double xDist = p.x - corner.x;
+		double yDist = p.y - corner.y;
+		return 0 < xDist && xDist < sideLength &&
+			   0 < yDist && yDist < sideLength;
+	}
+
+	/** 
+	* @return Whether point p part of/on the border of the square.
+	*/
+	public boolean isOn(Point p) {
+		// i dont know sorry
+	}
+	
+	/** 
+	* @param x How much to translate the sqaure by in the + x direction.
+	* @param y How much to translate the squarer by in the + y direction.
+	* @return The sqaure that results from the translation.
+	*/
+	public Square translate(double x, double y) {
+        return new Square(new Point(corner.x + x, corner.y + y), sideLength);
+	}
+
+	/** 
+	* @return The sqaure that results from scaling the side length and maintaining the bottom left corner
+	*/
+	public Square scale(double k) {
+		return new Square(corner, sideLength * k);
+	}
+
+	public Point[] corners() {
+        return new Point [] {
+		corner,
+        new Point(corner.x + sideLength, corner.y), 
+        new Point(corner.x, corner.y + sideLength), 
+        new Point(corner.x + sideLength, corner.y + sideLength)
+    };
+}
+
+	@Override
+	public String toString() {
+		return "(corner: " + corner + "; side length: " + sideLength + ")";
+	}
+}
