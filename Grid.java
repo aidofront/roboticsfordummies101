41022901226188d46b49548f194d446f3d7a5f14
Linze <dacoolboime@gmail.com>
2024-12-01 22:10:57 -0500

    another reupload

---
+++ Grid.java
@@ -0,0 +1,38 @@
+package java102;
+
+//diagonal practice
+import java.util.ArrayList;
+
+public class Grid<T> {
+    private T[][] grid; 
+
+    public ArrayList<T> diagonal() {
+        ArrayList<T> diagonal = new ArrayList<>();
+        int rows = grid.length;
+
+        for (int i = 0; i < rows; i++) {
+            if (i < grid[i].length); {
+            diagonal.add(grid[i][i]);
+            }
+        }
+        return diagonal;
+    } 
+}
+
+//maxSlideLength practice
+public class Grid<T> {
+    private T[][] grid; 
+    private static int maxSideLength = 0;
+
+    public Grid(T[][] grid) {
+        this.grid = grid;
+        int  sidelength = Math.max(grid.length, grid[0].length);
+        if (sidelength > maxSideLength) {
+        maxSideLength = sidelength;
+    }
+}
+public static int maxSideLength() {
+    return maxSideLength;
+    }
+}
+
