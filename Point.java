10518f6599de2dbdabe1a30c17594d95258fccd3
Linze <dacoolboime@gmail.com>
2024-12-02 21:35:17 -0500

    another ok

--- Point.java
+++ Point.java
@@ -53,4 +53,9 @@ public double angle (){
     return Math.toDegrees(angleInRadians);
 }
 
-
+//returnAll thing practice
+public static void returnAll(LibraryItem[] items) {
+    for (LibraryItem item : items) {
+        item.returnItem();
+    }
+}
